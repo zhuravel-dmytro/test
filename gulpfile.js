c5df64aab682ca299d338582dbba0f66cccb3e7e
Dimas <zhuravel16@gmail.com>
2019-08-21 18:39:47 +0300

    add

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 var gulp = require('gulp'),
-	less = require('gulp-less');
+	path = require('path'),
+  less = require('gulp-less'),
+  watch = require('gulp-watch');
+ 
 	
-	gulp.task('mytask', function() {
-    console.log('перевірка');
+	gulp.task('test', function() {
+
+console.log('працює');
+
 });
 
 	gulp.task('less', function () {
@@ -11,4 +16,10 @@ var gulp = require('gulp'),
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
     .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('watch', function () {
+  // Endless stream mode
+  return watch('app/less/**/*.less', { ignoreInitial: false })
+      .pipe(gulp.dest('build'));
 });
